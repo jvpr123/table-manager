06fbc8c263ddd5dfa3ac9219f66171d806e53b03
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-02 17:31:26 -0300

    test: add unit tests for Responsible domain entity

---
+++ tests/Unit/modules/Admin/Domain/Entity/ResponsibleEntityTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Modules\Shared\Domain\Entity;
+
+use Modules\Admin\Domain\Entity\ResponsibleEntity;
+use Modules\Shared\Domain\ValueObject\UUID;
+
+describe('Responsible Entity unit tests', function () {
+    beforeEach(function () {
+        $this->responsible = new ResponsibleEntity($this->name = 'responsible_name');
+    });
+
+    it('should generate a responsible entity successfully', function () {
+        expect($this->responsible->getId())->toBeInstanceOf(UUID::class);
+        expect($this->responsible->getCreatedAt()->toString())->toBe(now()->toString());
+        expect($this->responsible->getUpdatedAt()->toString())->toBe(now()->toString());
+    });
+
+    it('should retrieve responsible name successfully', function () {
+        expect($this->responsible->getName())->toBe($this->name);
+    });
+
+    it('should update responsible name successfully', function () {
+        $this->responsible->setName($name = 'updated_name');
+        expect($this->responsible->getName())->toBe($name);
+    });
+});
