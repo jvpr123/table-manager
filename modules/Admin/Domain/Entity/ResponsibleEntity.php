0cd0ce91de3643ab87b811d5f2b9446f0351c7b0
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-02 17:31:14 -0300

    feat: implement Responsible domain entity

---
+++ modules/Admin/Domain/Entity/ResponsibleEntity.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Admin\Domain\Entity;
+
+use Carbon\Carbon;
+use Modules\Shared\Domain\Entity\BaseEntity;
+use Modules\Shared\Domain\ValueObject\UUID;
+
+class ResponsibleEntity extends BaseEntity
+{
+    public function __construct(
+        private string $name,
+        ?UUID $id = null,
+        ?Carbon $createdAt = null,
+        ?Carbon $updatedAt = null
+    ) {
+        parent::__construct($id, $createdAt, $updatedAt);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
