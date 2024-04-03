9e185b33ca3f826835537b4774841c26f3cee632
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-03 12:54:20 -0300

    feat: implement delete responsible use-case

---
+++ modules/Admin/UseCase/DeleteResponsibleUseCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Admin\UseCase;
+
+use Modules\Admin\Gateway\ResponsibleGateway;
+use Modules\Shared\Exceptions\EntityNotFoundException;
+
+class DeleteResponsibleUseCase
+{
+    public function __construct(private ResponsibleGateway $responsibleRepository)
+    {
+    }
+
+    public function execute(string $id): void
+    {
+        $responsible = $this->responsibleRepository->find($id);
+
+        if (!$responsible) {
+            throw new EntityNotFoundException('Responsible', $id);
+        }
+
+        $this->responsibleRepository->delete($id);
+    }
+}
