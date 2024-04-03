77547f4a721e092c74fd8665f5978490389a4752
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-03 10:58:33 -0300

    feat: implement list responsibles use-case

---
+++ modules/Admin/UseCase/ListResponsiblesUseCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Admin\UseCase;
+
+use Modules\Admin\Domain\Entity\Responsible;
+use Modules\Admin\DTO\Responsible\ResponsibleOutputDTO;
+use Modules\Admin\Gateway\ResponsibleGateway;
+
+class ListResponsiblesUseCase
+{
+    public function __construct(private ResponsibleGateway $responsibleRepository)
+    {
+    }
+
+    /**
+     * @return ResponsibleOutputDTO[]
+     */
+    public function execute(): array
+    {
+        $responsibles = $this->responsibleRepository->list();
+
+        return array_map(fn (Responsible $responsible) => new ResponsibleOutputDTO(
+            id: $responsible->getId()->value,
+            name: $responsible->getName(),
+            createdAt: $responsible->getCreatedAt(),
+            updatedAt: $responsible->getUpdatedAt(),
+        ), $responsibles);
+    }
+}
