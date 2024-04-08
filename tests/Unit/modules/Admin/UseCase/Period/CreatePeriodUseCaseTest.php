018cfc3cf4c5e3ce76b6e058940619bd6dd84e9f
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-08 12:08:14 -0300

    test: add unit tests to create period use-case

---
+++ tests/Unit/modules/Admin/UseCase/Period/CreatePeriodUseCaseTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit\Modules\Admin\UseCase\Period;
+
+use Carbon\Carbon;
+use Modules\Admin\Domain\Entity\Period;
+use Modules\Admin\DTO\Period\CreatePeriodInputDTO;
+use Modules\Admin\DTO\Period\PeriodOutputDTO;
+use Modules\Admin\Gateway\PeriodGateway;
+use Modules\Admin\UseCase\Period\CreatePeriodUseCase;
+
+describe('CreatePeriodUseCase unit tests', function () {
+    beforeEach(function () {
+        $this->mockPeriodRepository = \Mockery::mock(PeriodGateway::class);
+        $this->useCase = new CreatePeriodUseCase($this->mockPeriodRepository);
+        $this->input = new CreatePeriodInputDTO(Carbon::createFromFormat('H:i', '8:30'));
+    });
+
+    it('should create a period successfully', function () {
+        $this->mockPeriodRepository
+            ->expects()
+            ->create(\Mockery::type(Period::class))
+            ->once();
+
+        $output = $this->useCase->execute($this->input);
+
+        expect($output)->toBeInstanceOf(PeriodOutputDTO::class);
+        expect($output->id)->toBeString();
+        expect($output->time)->toBe($this->input->time->format('H:i'));
+        expect($output->createdAt->toString())->toBe(now()->toString());
+        expect($output->updatedAt->toString())->toBe(now()->toString());
+    });
+
+    it('should throw exception on creating period error', function () {
+        $this->mockPeriodRepository
+            ->expects()
+            ->create(\Mockery::type(Period::class))
+            ->andThrow(new \Exception('Error creating period.'))
+            ->once();
+
+        expect(fn () => $this->useCase->execute($this->input))
+            ->toThrow(new \Exception('Error creating period.'));
+    });
+});
