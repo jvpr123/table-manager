d633ff1b1a97bb2aeb7aef46fd09f2ca4225b0cd
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-10 18:00:22 -0300

    test: add unit tests to local-meeting relationship

--- tests/Unit/modules/Admin/Domain/Entity/LocalEntityTest.php
+++ tests/Unit/modules/Admin/Domain/Entity/LocalEntityTest.php
@@ -3,13 +3,20 @@
 namespace Tests\Unit\Modules\Shared\Domain\Entity;
 
 use Modules\Admin\Domain\Entity\Local;
+use Modules\Admin\Domain\Entity\Meeting;
 use Modules\Shared\Domain\ValueObject\UUID;
+use Modules\Shared\Exceptions\EntityAlreadyRelatedException;
 
 describe('Local Entity unit tests', function () {
-    beforeEach(fn () => $this->local = new Local(
-        title: $this->title = 'local_title',
-        description: $this->description = 'local_description',
-    ));
+    beforeEach(function () {
+        $this->local = new Local(
+            title: $this->title = 'local_title',
+            description: $this->description = 'local_description',
+        );
+
+        $this->meetingA = new Meeting(now());
+        $this->meetingB = new Meeting(now()->addHour());
+    });
 
     it('should generate a local entity successfully', function () {
         expect($this->local->getId())->toBeInstanceOf(UUID::class);
@@ -33,5 +40,29 @@ describe('Local Entity unit tests', function () {
     it('should update local description successfully', function () {
         $this->local->setDescription($description = 'updated_description');
         expect($this->local->getDescription())->toBe($description);
+    });
+
+    it('should retrieve meetings ids related to local successfully', function () {
+        expect($this->local->getMeetingsIds())->toHaveCount(0);
+    });
+
+    it('should relate/unrelate a meeting id to local successfully', function () {
+        expect($this->local->getMeetingsIds())->toHaveCount(0);
+
+        expect($this->local->addMeeting($this->meetingA))->toBeNull();
+        expect($this->local->addMeeting($this->meetingB))->toBeNull();
+        expect($this->local->getMeetingsIds())->toHaveCount(2);
+
+        expect($this->local->removeMeeting($this->meetingB))->toBeNull();
+        expect($this->local->getMeetingsIds())->toHaveCount(1);
+    });
+
+    it('should throw exception when trying to add an already related meeting to local', function () {
+        $this->local->addMeeting($this->meetingA);
+        expect($this->local->getMeetingsIds())->toHaveCount(1);
+
+        expect(fn () => $this->local->addMeeting($this->meetingA))
+            ->toThrow(new EntityAlreadyRelatedException($this->local, $this->meetingA));
+        expect($this->local->getMeetingsIds())->toHaveCount(1);
     });
 });
