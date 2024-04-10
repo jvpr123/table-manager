6ea99b704f9fb52a95307dac6d4747242acf60b8
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-10 14:46:59 -0300

    test: add unit tests to responsible-entity relation

--- tests/Unit/modules/Admin/Domain/Entity/ResponsibleEntityTest.php
+++ tests/Unit/modules/Admin/Domain/Entity/ResponsibleEntityTest.php
@@ -2,12 +2,17 @@
 
 namespace Tests\Unit\Modules\Shared\Domain\Entity;
 
+use Modules\Admin\Domain\Entity\Meeting;
 use Modules\Admin\Domain\Entity\Responsible;
 use Modules\Shared\Domain\ValueObject\UUID;
+use Modules\Shared\Exceptions\EntityAlreadyRelatedException;
 
 describe('Responsible Entity unit tests', function () {
     beforeEach(function () {
         $this->responsible = new Responsible($this->name = 'responsible_name');
+
+        $this->meetingA = new Meeting(now());
+        $this->meetingB = new Meeting(now()->addHour());
     });
 
     it('should generate a responsible entity successfully', function () {
@@ -23,5 +28,29 @@ describe('Responsible Entity unit tests', function () {
     it('should update responsible name successfully', function () {
         $this->responsible->setName($name = 'updated_name');
         expect($this->responsible->getName())->toBe($name);
+    });
+
+    it('should retrieve meetings ids related to responsible successfully', function () {
+        expect($this->responsible->getMeetingsIds())->toHaveCount(0);
+    });
+
+    it('should relate/unrelate a meeting id to responsible successfully', function () {
+        expect($this->responsible->getMeetingsIds())->toHaveCount(0);
+
+        expect($this->responsible->addMeeting($this->meetingA))->toBeNull();
+        expect($this->responsible->addMeeting($this->meetingB))->toBeNull();
+        expect($this->responsible->getMeetingsIds())->toHaveCount(2);
+
+        expect($this->responsible->removeMeeting($this->meetingB))->toBeNull();
+        expect($this->responsible->getMeetingsIds())->toHaveCount(1);
+    });
+
+    it('should throw exception when trying to add an already related meeting to responsible', function () {
+        $this->responsible->addMeeting($this->meetingA);
+        expect($this->responsible->getMeetingsIds())->toHaveCount(1);
+
+        expect(fn () => $this->responsible->addMeeting($this->meetingA))
+            ->toThrow(new EntityAlreadyRelatedException($this->responsible, $this->meetingA));
+        expect($this->responsible->getMeetingsIds())->toHaveCount(1);
     });
 });
