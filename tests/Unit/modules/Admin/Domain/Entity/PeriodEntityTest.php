f3042ff24c501ce8f8f382e583c50cf102f51aa8
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-10 17:54:17 -0300

    test: add unit tests to period-meetings relationships

--- tests/Unit/modules/Admin/Domain/Entity/PeriodEntityTest.php
+++ tests/Unit/modules/Admin/Domain/Entity/PeriodEntityTest.php
@@ -3,13 +3,18 @@
 namespace Tests\Unit\Modules\Shared\Domain\Entity;
 
 use Carbon\Carbon;
+use Modules\Admin\Domain\Entity\Meeting;
 use Modules\Admin\Domain\Entity\Period;
 use Modules\Shared\Domain\ValueObject\UUID;
+use Modules\Shared\Exceptions\EntityAlreadyRelatedException;
 
 describe('Period Entity unit tests', function () {
     beforeEach(function () {
         $this->time = Carbon::createFromFormat('H:i', '08:30');
         $this->period = new Period($this->time);
+
+        $this->meetingA = new Meeting(now());
+        $this->meetingB = new Meeting(now()->addHour());
     });
 
     it('should generate a period entity successfully', function () {
@@ -26,5 +31,29 @@ describe('Period Entity unit tests', function () {
         $time = Carbon::createFromFormat('H:i', '08:45');
         $this->period->setTime($time);
         expect($this->period->getTime())->toBe($time->format('H:i'));
+    });
+
+    it('should retrieve meetings ids related to period successfully', function () {
+        expect($this->period->getMeetingsIds())->toHaveCount(0);
+    });
+
+    it('should relate/unrelate a meeting id to period successfully', function () {
+        expect($this->period->getMeetingsIds())->toHaveCount(0);
+
+        expect($this->period->addMeeting($this->meetingA))->toBeNull();
+        expect($this->period->addMeeting($this->meetingB))->toBeNull();
+        expect($this->period->getMeetingsIds())->toHaveCount(2);
+
+        expect($this->period->removeMeeting($this->meetingB))->toBeNull();
+        expect($this->period->getMeetingsIds())->toHaveCount(1);
+    });
+
+    it('should throw exception when trying to add an already related meeting to period', function () {
+        $this->period->addMeeting($this->meetingA);
+        expect($this->period->getMeetingsIds())->toHaveCount(1);
+
+        expect(fn () => $this->period->addMeeting($this->meetingA))
+            ->toThrow(new EntityAlreadyRelatedException($this->period, $this->meetingA));
+        expect($this->period->getMeetingsIds())->toHaveCount(1);
     });
 });
