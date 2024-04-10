fdb582c992b98f0bcadd82b89caae06653e3f83a
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-04-10 18:00:07 -0300

    refactor: add local-meeting domain entities relationship

--- modules/Admin/Domain/Entity/Local.php
+++ modules/Admin/Domain/Entity/Local.php
@@ -5,9 +5,17 @@ namespace Modules\Admin\Domain\Entity;
 use Carbon\Carbon;
 use Modules\Shared\Domain\Entity\BaseEntity;
 use Modules\Shared\Domain\ValueObject\UUID;
+use Modules\Shared\Exceptions\EntityAlreadyRelatedException;
 
+/**
+ * @property string[] $meetingsIds
+ *
+ * @package Local
+ */
 class Local extends BaseEntity
 {
+    private array $meetingsIds = [];
+
     public function __construct(
         private string $title,
         private string $description,
@@ -36,5 +44,30 @@ class Local extends BaseEntity
     public function setDescription(string $description): void
     {
         $this->description = $description;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getMeetingsIds(): array
+    {
+        return $this->meetingsIds;
+    }
+
+    public function addMeeting(Meeting $meeting): void
+    {
+        $meetingAlreadyRelated = in_array($meeting->getId()->value, $this->meetingsIds);
+
+        if ($meetingAlreadyRelated) {
+            throw new EntityAlreadyRelatedException($this, $meeting);
+        }
+
+        array_push($this->meetingsIds, $meeting->getId()->value);
+    }
+
+    public function removeMeeting(Meeting $meeting): void
+    {
+        $index = array_search($meeting->getId()->value, $this->meetingsIds);
+        unset($this->meetingsIds[$index]);
     }
 }
