016f7e00d607680898ebefc062cbe733d84047d7
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-07-25 12:21:11 -0300

    test: add feature tests to local update route

--- tests/Feature/LocalControllerTest.php
+++ tests/Feature/LocalControllerTest.php
@@ -18,7 +18,7 @@ describe('LocalController::store()', function () {
         $response->assertJson(fn (AssertableJson $json) => $json->hasAll(['message', 'local']));
     });
 
-    it('should return 422 HTTP status-code with validation errors', function () {
+    it('should return 400 HTTP status-code with validation errors', function () {
         $bodyData = [
             'title' => null,
             'description' => 50,
@@ -84,5 +84,46 @@ describe('LocalController::index()', function () {
         $responseData = $response->getOriginalContent();
         expect($responseData['message'])->toBe('Locals found successfully.');
         expect($responseData['locals'])->toBeEmpty();
+    });
+});
+
+describe('LocalController::update()', function () {
+    beforeEach(function () {
+        $this->local = Local::factory()->create();
+        $this->route = route('update-local', ['localId' => $this->local->uuid]);
+    });
+
+    it('should return 200 HTTP status-code with updated local', function () {
+        $bodyData = Local::factory()->make()->toArray();
+
+        $response = $this->putJson($this->route, $bodyData);
+        $response->assertOk();
+
+        $responseData = $response->getOriginalContent();
+        expect($responseData['message'])->toBe('Local updated successfully.');
+        expect($responseData['local']->id)->toBe($this->local->uuid);
+        expect($responseData['local']->title)->toBe($bodyData['title']);
+        expect($responseData['local']->description)->toBe($bodyData['description']);
+    });
+
+    it('should return 400 HTTP status-code with validation errors', function () {
+        $bodyData = [
+            'title' => null,
+            'description' => 50,
+        ];
+
+        $response = $this->putJson($this->route, $bodyData);
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertSee('Validation error.');
+        $response->assertJson(fn (AssertableJson $json) => $json->hasAll(['message', 'errors']));
+    });
+
+    it('should return 404 HTTP status-code if local not found', function () {
+        $route = route('update-local', $id = uuid_create());
+        $bodyData = Local::factory()->make()->toArray();
+
+        $response = $this->putJson($route, $bodyData);
+        $response->assertNotFound();
+        $response->assertSee("Local not found using ID $id provided.");
     });
 });
