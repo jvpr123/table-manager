5f57d346fe3918f750d2e118c311ae14a368d1ff
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-07-29 13:00:13 -0300

    test: add unit tests to relate meeting group to period repository

--- database/factories/MeetingGroupFactory.php
+++ database/factories/MeetingGroupFactory.php
@@ -9,6 +9,7 @@ class MeetingGroupFactory extends Factory
     public function definition(): array
     {
         return [
+            'uuid' => uuid_create(),
             'name' => fake()->word(),
             'description' => fake()->words(asText: true),
         ];
--- tests/Unit/modules/Admin/Respository/MeetingGroupRepositoryTest.php
+++ tests/Unit/modules/Admin/Respository/MeetingGroupRepositoryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Modules\Admin\Repository;
 
 use App\Models\MeetingGroup as MeetingGroupModel;
+use App\Models\Period;
 use Modules\Admin\Domain\Entity\MeetingGroup;
 use Modules\Admin\Repository\MeetingGroupRepository;
 
@@ -36,5 +37,32 @@ describe('MeetingGroupRepository create() unit tests', function () {
         })->toThrow(new \Exception('Error saving meeting group.'));
 
         $this->assertDatabaseMissing('meeting_groups', ['uuid' => $this->meetingGroup->getId()->value]);
+    });
+});
+
+describe('MeetingGroupRepository relatePeriods() unit tests', function () {
+    beforeEach(function () {
+        $this->periods = Period::factory()->count(5)->create();
+        $this->meetingGroup = MeetingGroupModel::factory()->create();
+        $this->meetingGroup->periods()->save($this->periods[0]);
+
+        $this->repository = new MeetingGroupRepository();
+    });
+
+    it('should relate Periods to Meeting Group without detaching successfully', function () {
+        $periodsIds = $this->periods->map(fn (Period $period) => $period->uuid);
+        $output = $this->repository->relatePeriods(
+            meetingGroupId: $this->meetingGroup->uuid,
+            periodsIds: $periodsIds->toArray(),
+        );
+
+        expect($output)->toBeNull();
+        expect($this->meetingGroup->periods)->toHaveCount($this->periods->count());
+        $periodsIds->map(function (string $periodId) {
+            $this->assertDatabaseHas('meeting_group_periods', [
+                'meeting_group_id' => $this->meetingGroup->uuid,
+                'period_id' => $periodId,
+            ]);
+        });
     });
 });
